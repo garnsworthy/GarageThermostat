fdfe0b5c357da2ea33ad7deb07b42c751fb842b6
Garnsworthy <Chris.Garnsworthy@trane.com>
2022-11-23 15:16:22 -0600

    maybe the emedded app works?  I don't know will test tonight.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GarageThermostat
+
+Variables:
+ - temp, temp in deg C * 100
+ - hum, humidity in % *100
+ - heat, 0/1 if set to ON
+ - heating, 0/1 if furnace is heating
+ - setpt, temp in deg C * 100
--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -1,18 +1,22 @@
-#include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
-#include <ESP8266WiFi.h>
+#include <ArduinoHttpClient.h>
+#include <Adafruit_ESP8266.h>
+#include "DHTesp.h"      // Click here to get the library: http://librarymanager/All#DHTesp
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>
 
 DHTesp dht;
 
 const char *ssid = "asdf";
 const char *password = "asdf";
-const char *host = "192.168.1.55";
+const char *host = "http://192.168.1.55";
+const char *path = "data";
 const uint16_t port = 8085;
 
 int temperature;     // C * 100
 int humidity;        // % * 100
 int setPoint = 1278; // C * 100 (55 F)
 boolean heat = false;
+boolean heating = false;
 
 void setup()
 {
@@ -30,18 +34,6 @@ void setup()
     // if you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
   }
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin(ssid, password);
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-  // Serial.println("");
-  // Serial.println("WiFi connected");
-  // Serial.println("IP address: ");
-  // Serial.println(WiFi.localIP());
-
   dht.setup(D1, DHTesp::DHT22); // Connect DHT sensor to GPIO 17
 }
 
@@ -57,64 +49,37 @@ void loop()
   { // once a minute for now.
     // Use WiFiClient class to create TCP connections
     WiFiClient client;
-    if (!client.connect(host, port))
-    {
-      Serial.println("connection failed");
-      delay(5000);
-      return;
-    }
-
-    // This will send a string to the server
-    Serial.println("sending data to server");
-    sendData(client);
-    readData(client);
-    // Close the connection
-    Serial.println();
-    Serial.println("closing connection");
-    client.stop();
+    HttpClient http = HttpClient(client, host, port);
+    http.beginRequest();
+    sendData(http);
+    readData(http);
   }
 }
 
-void sendData(WiFiClient client)
+void sendData(HttpClient http)
 {
-  if (client.connected())
-  {
-    client.print("{temp:");
-    client.print(temperature);
-    client.print(",");
-    client.print("hum:");
-    client.print(humidity);
-    client.print(",");
-    client.print("stpt:");
-    client.print(setPoint);
-    client.print(",");
-    client.print("heat:");
-    client.print(heat);
-    client.println("}");
-  }
+  StaticJsonDocument<200> doc;
+  doc["temp"] = temperature;
+  doc["hum"] = humidity;
+  doc["heating"] = heating;
+  doc["setpt"] = setPoint;
+
+  String json;
+  serializeJson(doc, json);
+
+  //  http.POST(json);
+  http.post(path);
+  http.beginBody();
+  http.print(json);
+  http.endRequest();
 }
 
-void readData(WiFiClient client)
+void readData(HttpClient http)
 {
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0)
-  {
-    if (millis() - timeout > 5000)
-    {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
-    }
-  }
-
   // Read all the lines of the reply from server and print them to Serial
   Serial.println("receiving from remote server");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available())
-  {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
-  }
+  String res = http.responseBody();
+
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, res);
 }
